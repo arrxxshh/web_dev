eb672012b4dd81dd3aaaf5afed2b7fbf7b6c9877
Arshpreet Singh <116718117+arrxxshh@users.noreply.github.com>
2022-12-03 20:11:30 +0530

    Add files via upload
    font file not uploaded

---
+++ project_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+include 'project_login.html';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if ($conn) {
+    
+  }
+  
+  ?>
+
+</body>
+</html>
